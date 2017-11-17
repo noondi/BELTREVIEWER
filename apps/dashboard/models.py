dd450f473514211c79e4e95e68d4feac137ca6da
noondi <labeta.nati@tutanota.com>
2017-11-16 20:25:30 -0500

    third commit

--- apps/dashboard/models.py
+++ apps/dashboard/models.py
@@ -1,25 +1,28 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.db import models
+from ..beltbelt.models import User
 
 
 
-
+# Create your models here.
 class Author(models.Model):
-    full_name = models.CharField(max_length=255)    
-    created_at = models.CharField(auto_now_add = True)
-    updated_at = models.CharField(auto_now_add = True)
+    full_name = models.CharField(max_length =255)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now_add = True)
+
 
 class Book(models.Model):
-    title = models.CharField(max_length=255)    
-    created_at = models.CharField(auto_now_add = True)
-    updated_at = models.CharField(auto_now_add = True)
-    author = models.ForeignKey(Author, related_name='books')
-    uploader = models.ForeignKey(Author, related_name='books')
+    title = models.CharField(max_length =255)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now_add = True)
+    author = models.ForeignKey(Author, related_name="books")
+    uploader = models.ForeignKey(User, related_name="uploaded_book")
+
 class Review(models.Model):
-    comment = models.CharField(max_length=255)    
-    rating = models.CharField(auto_now_add = True)
-    created_at = models.CharField(auto_now_add = True)
-    updated_at = models.CharField(auto_now_add = True)
-    reviewer = models.ForeignKey(Author, related_name='user_review')
-    book_reviewed = models.ForeignKey(Author, related_name='books')
-    
+    comment = models.TextField()
+    rating = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now_add = True)
+    reviewer = models.ForeignKey(User, related_name="user_review")
+    book_reviewed = models.ForeignKey(Book, related_name="review_of_book")
